ac3c6aa3ead406212af3e04c53823ff874028a1c
nlfox <nlfox@msn.cn>
2017-02-02 14:43:01 +0000

    Template Engine Test added

--- Task1.java
+++ Task1.java
@@ -32,14 +32,14 @@ public class Task1 {
     @Test
     public void testEntryMapSpec1() {
         exception.expect(RuntimeException.class);
-        map.store(null,"a",true);
+        map.store(null, "a", true);
     }
 
     //spec2 - Replace value string cannot be NULL. A runtime exception is thrown otherwise.
     @Test
     public void testEntryMapSpec2() {
         exception.expect(RuntimeException.class);
-        map.store("test",null,true);
+        map.store("test", null, true);
     }
 
 
@@ -50,13 +50,12 @@ public class Task1 {
         map.store("name", "Adam", null);
         map.store("surname", "Dykes", null);
         map.store("age", "29", null);
-        String result = engine.evaluate("Hello ${name}, is your age ${age ${symbol}}", map,"delete-unmatched");
+        String result = engine.evaluate("Hello ${name}, is your age ${age ${symbol}}", map, "delete-unmatched");
         assertEquals("Hello Adam, is your age 29", result);
     }
 
     //    spec4 - The entries are ordered and follow the order in which they appear in the program.
     //    ---> In the previous example the "name"/"Adam" entry is the first entry since it was stored first.
-
     @Test
     public void testEntryMapSpec4() {
         map.store("surname", "Dykes", false);
@@ -65,9 +64,38 @@ public class Task1 {
 
     }
 
+//    spec5 - Entries that already exist cannot be stored again.
+//            ---> In the following commands only the first entry will be saved:
+//            map.store("name", "Adam", false);
+//                    map.store("name", "Adam", false);
+//            ---> In the following commands, however, both entries will be saved:
+//            map.store("name", "Adam", false);
+//                    map.store("name", "Adam", true);
 
+    @Test
+    public void testEntryMapSpec5() {
 
+    }
 
+    private String[] tOps = new String[]{"keep-unmatched", "delete-unmatched"};
+
+    //spec1 - The template string can be NULL or empty. If template string NULL or empty, then the unchanged template string is returned.
+    @Test
+    public void testTemplateEngineSpec1() {
+        for (String op : tOps) {
+            assertEquals("", engine.evaluate("", map, op));
+            assertEquals(null, engine.evaluate(null, map, op));
+        }
+    }
+
+    //spec2 - The EntryMap object can be NULL. If EntryMap object NULL, then the unchanged template string is returned.
+    @Test
+    public void testTemplateEngineSpec2() {
+        for (String op : tOps) {
+            assertEquals("", engine.evaluate("", null, op));
+            assertEquals("test2333", engine.evaluate("test2333", map, op));
+        }
+    }
 
 
 
