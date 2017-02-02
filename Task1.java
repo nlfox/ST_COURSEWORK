06d97deb50acedba695803b3d68888bf90c91f4f
nlfox <nlfox@msn.cn>
2017-02-02 10:52:42 +0000

    Task1 spec1

--- Task1.java
+++ Task1.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import st.EntryMap;
 import st.TemplateEngine;
 
@@ -9,10 +12,21 @@ public class Task1 {
     EntryMap map;
     TemplateEngine engine;
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Before
     public void setUp() throws Exception {
         map = new EntryMap();
         engine = new TemplateEngine();
+    }
+
+    @Test
+    public void testEntryMapSpec1() {
+        exception.expect(RuntimeException.class);
+        map.store(null,"a",true);
+        map.store(null,null,true);
+
     }
 
 
