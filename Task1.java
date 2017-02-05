5d14b9d6d7f822bfd6c1dcf71eca924ecf0a5c29
urasian_electron <jeffrey032743@gmail.com>
2017-02-05 13:37:23 +0000

    added more test cases

--- Task1.java
+++ Task1.java
@@ -121,5 +121,78 @@ public class Task1 {
 
     }
 
+    //spec3 - Matching mode cannot be NULL and must be one of the possible values ("keep-unmatched" and "delete-unmatched").
+    //If matching mode NULL or other value, it defaults to "delete-unmatched".
+    @Test
+    public void testTemplateEngineSpec3(){
+        map.store("name","Adam",false);
+        assertEquals("Adam",engine.evaluate("${hello}${name}",map,null));
+    }
+
+    //spec4 - Templates in a template string occur between "${" and "}".
+    // In a template, everything between its boundaries ("${" and "}") is treated as normal text when matched against an entry.
+//            ---> In the template string "Hello ${name}, could you please give me your ${item} ?" the two templates are:
+//            1 - ${name}
+//            2 - ${item}
+//            ---> The text of each template that will be matched against the EntryMap stored entries are:
+//            1 - "name"
+//            2 - "item"
+//            (i.e. the template boundaries are omitted)
+    @Test
+    public void testTemplateEngineSpec4(){
+        map.store("⁄€‹›ﬁﬂ‡°","˝ÓÔÒ",true);
+        assertEquals("${⁄€‹›ﬁﬂ‡°",engine.evaluate("${⁄€‹›ﬁﬂ‡°",map,pMode));
+        assertEquals("⁄€‹›ﬁﬂ‡°}",engine.evaluate("⁄€‹›ﬁﬂ‡°}",map,pMode));
+        assertEquals("˝ÓÔÒ",engine.evaluate("${⁄€‹›ﬁﬂ‡°}",map,pMode));
+
+    }
+
+    //spec5 - When a template is matched against an entry key, any non visible character does not affect the result.
+//            ---> The entry "middle name"/"Peter" will match all of the following templates:
+//            1 - ${middle name}
+//                2 - ${middlename}
+//                3 - ${middle       name}
+    @Test
+    public void testTemplateEngineSpec5(){
+        map.store("my life","x",false);
+        assertEquals("x",engine.evaluate("${my\t \b\n\r\flife}",map,pMode));
+    }
+    //spec6 - In a template string every "${" and "}" occurrence acts as a boundary of at MOST one template.
+//            ---> Processing from left-to-right, each "}" occurrence that is not already a boundary to a template is matched to its closest preceding "${" occurrence which also is not already a boundary to a template.
+//            ---> In the template string "I heard that }: ${name} said: ${we should try or best for winning the ${competition} cup.}" the templates are:
+//            1 - ${name}
+//                2 - ${competition}
+//                3 - ${we should try or best for winning the ${competition} cup.}
+
+//    spec7 - In a template string the different templates are ordered according to their length. The shorter templates precede.
+//            ---> In the case of same-length templates, the one that occurs first when traversing the template string from left-to-right precedes.
+//            ---> In the template string "abc}${de}${fgijk${lm}nopqr}${s}uvw${xyz" the sorted templates are:
+//            1 - ${s}
+//                2 - ${de}
+//                3 - ${lm}
+//                4 - ${fgijk${lm}nopqr}
+
+
+//    spec8 - The engine processes one template at a time and attempts to match it against the keys of the EntryMap entries until there is a match or the entry list is exhausted.
+//            ---> The engine processes both templates and entries according to their order.
+//            ---> If there is a match:
+//            1 - The template (including its boundaries) in the template string is replaced by the value of the matched entry.
+//            2 - The same replace happens to all other templates which include the replaced template.
+//                    3 - The template engine moves on to the next template and repeats.
+//            ---> If the entry list is exhausted and no match found for the current template:
+//            1 - The template engine just moves on to the next template if matching the mode is "keep-unmatched".
+//            2 - The engine deletes the unmatched template from the template string and all other templates which include it.
+    @Test
+    public void testTemplateEngineSpec8_step2(){
+        EntryMap map = new EntryMap();
+        TemplateEngine engine = new TemplateEngine();
+
+        map.store("name", "Adam", false);
+        map.store("surname", "Dykes", false);
+        map.store("age ${y}", "29", false);
+        map.store("y","24",false);
+
+        assertEquals("Hello Adam, is your age 29 24",engine.evaluate("Hello ${name}, is your age ${age 24 ${symbol}}", map,"delete-unmatched"));
+    }
 
 }
